e21dd556d546dd10ca983be81513e143d865948d
LylaChapman-10 <821587@mystma.org>
2022-09-21 14:18:07 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-#WRITE YOUR CODE IN THIS FILE
+def greaterThan(x,y):
+    if x > y:
+        return  True
+    else:
+        return False
+def smallerThan(x,y):
+    if x < y:
+        return True
+    else:
+        return False
+def equalTo(x,y):
+    if x == y:
+        return True
+    else:
+        return False
+def greaterOrEqual(x,y):
+    if x >= y:
+        return True
+    else:
+        return False
+def smallerOrEqual(x,y)
+    if x <= y:
+        return True
+    else:
+        return False 
